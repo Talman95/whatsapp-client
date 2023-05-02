d588ad8d6fec71565e6e4bf508a93e2f5c3546ac
Talman95 <taltro528@gmail.com>
2023-05-02 20:38:16 +0300

    added appSlice

---
+++ src/app/appSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+const slice = createSlice({
+  name: 'app',
+  initialState: {
+    error: null as string | null,
+    isLoading: false,
+    isAppInitialized: false,
+  },
+  reducers: {
+    setError(state, action: PayloadAction<{ error: string | null }>) {
+      state.error = action.payload.error;
+    },
+    setIsLoading(state, action: PayloadAction<{ isLoading: boolean }>) {
+      state.isLoading = action.payload.isLoading;
+    },
+    setIsAppInitialized(state, action: PayloadAction<{ isAppInitialized: boolean }>) {
+      state.isAppInitialized = action.payload.isAppInitialized;
+    },
+  },
+});
+
+export const { actions: appActions, reducer: appReducer } = slice;
